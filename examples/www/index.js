b04ad7f7bb8092e6fdf9c88a53b5b85e3acd4e49
devfans <stefanliu@outlook.com>
2019-09-23 14:54:06 +0800

    Add initial project structure

---
+++ examples/www/index.js
@@ -0,0 +1,3 @@
+import * as wand_example from "wand-example";
+
+wand_example.greet();
---
+++ src/scene.rs
@@ -0,0 +1,7 @@
+pub fn draw_scene () {
+    println!("Drawing scenine");
+}
+
+pub fn get_scene_name() -> String {
+    String::from("ss")
+}
