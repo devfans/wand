f6742b1df98d9ee5382daf322b8aab0a432b7dcb
devfans <stefanliu@outlook.com>
2019-10-08 11:37:13 +0800

    Add prelude and roll crate version

--- src/prelude.rs
+++ src/prelude.rs
@@ -1,16 +1,3 @@
-#[macro_use] pub mod traits;
-#[macro_use] pub mod utils;
-
-pub mod core;
-pub mod scene;
-pub mod container;
-pub mod span;
-pub mod component;
-pub mod section;
-pub mod content;
-pub mod input;
-pub mod prelude;
-
 pub use crate::core::{Application, State, FpsCounter};
 pub use crate::scene::Scene;
 pub use crate::container::Container;
